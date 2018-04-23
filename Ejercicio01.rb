bcad9996022a460820eddd5ba6f4c6db7e6c9e18
cristian vidal <cristian.vidal.lopez@gmail.com>
2018-04-23 01:31:43 -0300

    ADD: first commit

---
+++ Ejercicio01.rb
@@ -0,0 +1,18 @@
+class Table
+  attr_accessor :tname, :collect
+  def initialize(tname, *collect)
+    @tname = tname
+    @collect = collect.map(& :to_i)
+  end
+
+  def self.create_table
+  tables = []
+  lines = File.open('casino.txt', 'r').readlines
+  lines.each do |line|
+    new_line = line.split(', ')
+    tables << Table.new(new_line.shift, *new_line)
+  end
+  tables
+  end
+end
+puts Table.create_table
